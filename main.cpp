e453af246411c106c3d927e8808cc9f3254caf31
danila0606 <danila.zubko@bk.ru>
2020-05-03 17:30:10 +0300

    version 1.0

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "tree.h"
+
+
+int main() {
+
+    try {
+        istringstream str("sin(x)");
+        Node *expression = Read_str_to_tree(str, 0);
+        tree_print(expression);
+        cout <<endl;
+        
+        Node *derivative = Diff(expression);
+        tree_print(derivative);
+        cout <<endl;
+
+        CountVal(derivative, 2);
+
+    } catch (exception &e) {
+        cerr << e.what()<<endl;
+        exit(1);
+    }
+    
+    return 0;
+}
+
+
